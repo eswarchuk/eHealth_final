e3c859b7a3204d82407355fbe2c57b435061ae85
eswarchuk <32390736+eswarchuk@users.noreply.github.com>
2017-10-01 21:08:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,19 +7,22 @@ var app      = express();
 var port     = process.env.PORT || 8080;
 var http 	= require ('http'); 
 var mongoose = require('mongoose');
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };   
 var passport = require('passport');
 var flash    = require('connect-flash');
 
 var configDB = require('./config/database.js');
 
 // configuration ===============================================================
-mongoose.connect(configDB.url, function (err, res) {
-      if (err) {
-      console.log ('ERROR connecting to: ' + configDB.url + '. ' + err);
-      } else {
-      console.log ('Succeeded connected to: ' + configDB.url);
-      }
-    });
+mongoose.connect(configDB.url, options);
+var conn = mongoose.connection;             
+ 
+conn.on('error', console.error.bind(console, 'connection error:'));  
+ 
+conn.once('open', function() {
+  // Wait for the database connection to establish, then start the app.                         
+});
 // connect to our database
 
 require('./config/passport')(passport); // pass passport for configuration
