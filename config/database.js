08a9c2332eda94d27283d54844bd979b0bbfbfe2
eswarchuk <32390736+eswarchuk@users.noreply.github.com>
2017-10-01 10:21:12 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://localhost:27017/eHealth' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://ehealth:ehealth@cluster0-shard-00-00-dohw6.mongodb.net:27017,cluster0-shard-00-01-dohw6.mongodb.net:27017,cluster0-shard-00-02-dohw6.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
