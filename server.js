930bcc4f3997d9011b69a15c94b29bb0c41149d4
eswarchuk <32390736+eswarchuk@users.noreply.github.com>
2017-10-01 10:34:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express  = require('express');
 var app      = express();
 var port     = process.env.PORT || 8080;
 var mongoose = require('mongoose');
-var MongoClient = require('mongodb').MongoClient,
+var MongoClient = require('mongodb').MongoClient;
 var passport = require('passport');
 var flash    = require('connect-flash');
 
