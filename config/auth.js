8611d7996efcf3801883d92d31b825c3316d5e2f
eswarchuk <32390736+eswarchuk@users.noreply.github.com>
2017-10-01 12:22:18 +0200

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -12,13 +12,13 @@ module.exports = {
 	'twitterAuth' : {
 		'consumerKey' 		: 'MSdvlixzCMhkmaoGNtRk83Ryl',
 		'consumerSecret' 	: 'nctAgm780FhR0tgLOk6A582opXvFjYXkLTuPjHNfKzAMsiUqeI',
-		'callbackURL' 		: 'http://localhost:8080/auth/twitter/callback'
+		'callbackURL' 		: 'https://serene-coast-24572.herokuapp.com/auth/twitter/callback'
 	},
 
 	'googleAuth' : {
 		'clientID' 		: '656346168053-anoch56oufcd1teej328cf948p9hgh1d.apps.googleusercontent.com',
 		'clientSecret' 	: 'YJZG9WJIqr20ZMC3ZXtEjMIM',
-		'callbackURL' 	: 'http://localhost:8080/auth/google/callback'
+		'callbackURL' 	: 'https://serene-coast-24572.herokuapp.com/auth/google/callback'
 	}
 
 };
