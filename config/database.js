1e5770100456a912eecfc4eccff291f4a122df97
eswarchuk <32390736+eswarchuk@users.noreply.github.com>
2017-10-02 15:00:30 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://heroku_32w6x80p:Roopsilu1008@ds157614.mlab.com:57614/heroku_32w6x80p/ehealth_database'
+	'url' : 'mongodb://eswar:eswardb@celtest1.lnu.se:20762/eswardb'
 	
 
 };
