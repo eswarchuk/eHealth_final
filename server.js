cb7f995d2b6c4b618e38a273baafd8254550a951
eswarchuk <32390736+eswarchuk@users.noreply.github.com>
2017-10-08 11:44:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,24 +11,23 @@ var flash    = require('connect-flash');
 var configDB = require('./config/database.js');
 
 // configuration ===============================================================
-
+// connect to our database
 mongoose.connect(configDB.url);
 		 //{ useMongoClient: true, promiseLibrary: global.Promise });
-// connect to our database
+
 
 require('./config/passport')(passport); // pass passport for configuration
 
 app.configure(function() {
 
-	// set up our express application
+	// set up express application
 	app.use(express.logger('dev')); // log every request to the console
 	app.use(express.cookieParser()); // read cookies (needed for auth)
 	app.use(express.bodyParser()); // get information from html forms
-
 	app.set('view engine', 'ejs'); // set up ejs for templating
 
 	// required for passport
-	app.use(express.session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+	app.use(express.session({ secret: 'hopedieslast' })); // session secret
 	app.use(passport.initialize());
 	app.use(passport.session()); // persistent login sessions
 	app.use(flash()); // use connect-flash for flash messages stored in session
@@ -36,8 +35,8 @@ app.configure(function() {
 });
 
 // routes ======================================================================
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+require('./app/routes.js')(app, passport); // load our routes and import app and passport from routes.js
 
 // launch ======================================================================
 app.listen(port);
-console.log('The magic happens on port ' + port);
+console.log('Ding Dong ' + port);
