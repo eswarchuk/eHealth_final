9723035341a1f00ba2ed8fcd7eae9ee1d4dab8f7
eswarchuk <32390736+eswarchuk@users.noreply.github.com>
2017-10-08 12:02:03 +0200

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -29,9 +29,95 @@ module.exports = function(passport) {
         });
     });
 
+// LOCAL LOGIN =============================================================
     
+    passport.use('local-login', new LocalStrategy({
+        // override local strategy defaults username and password with email
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true // check if a user is logged in or not
+    },
+    function(req, email, password, done) {
+
+        // asynchronous
+        process.nextTick(function() {
+            User.findOne({ 'local.email' :  email }, function(err, user) {
+                // if there are any errors, return the error
+                if (err)
+                    return done(err);
+
+                // if no user is found, return the message
+                if (!user)
+                    return done(null, false, req.flash('loginMessage', 'No user found.'));
+
+                if (!user.validPassword(password))
+                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+
+                // all is well, return user
+                else
+                    return done(null, user);
+            });
+        });
+
+    }));
+
+    // LOCAL SIGNUP ============================================================
     
-   
+    passport.use('local-signup', new LocalStrategy({
+        // override local strategy defaults username and password with email
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true // check if a user is logged in or not
+    },
+    function(req, email, password, done) {
+
+        // asynchronous
+        process.nextTick(function() {
+
+            //  check if the email address is in use.
+            User.findOne({'local.email': email}, function(err, existingUser) {
+
+                // if there are any errors, return the error
+                if (err)
+                    return done(err);
+
+                // check if there's already a user with that email
+                if (existingUser) 
+                    return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+
+                //  If we're logged in, we're connecting a new local account.
+                if(req.user) {
+                    var user            = req.user;
+                    user.local.email    = email;
+                    user.local.password = user.generateHash(password);
+                    user.save(function(err) {
+                        if (err)
+                            throw err;
+                        return done(null, user);
+                    });
+                } 
+                //  We're not logged in, so we're creating a brand new user.
+                else {
+                    // create the user
+                    var newUser            = new User();
+
+                    newUser.local.email    = email;
+                    newUser.local.password = newUser.generateHash(password);
+
+                    newUser.save(function(err) {
+                        if (err)
+                            throw err;
+
+                        return done(null, newUser);
+                    });
+                }
+
+            });
+        });
+
+    }));
+
+       
     // FACEBOOK ================================================================
    
     passport.use(new FacebookStrategy({
@@ -260,92 +346,3 @@ module.exports = function(passport) {
 
 };
  
-// LOCAL LOGIN =============================================================
-    
-    passport.use('local-login', new LocalStrategy({
-        // override local strategy defaults username and password with email
-        usernameField : 'email',
-        passwordField : 'password',
-        passReqToCallback : true // check if a user is logged in or not
-    },
-    function(req, email, password, done) {
-
-        // asynchronous
-        process.nextTick(function() {
-            User.findOne({ 'local.email' :  email }, function(err, user) {
-                // if there are any errors, return the error
-                if (err)
-                    return done(err);
-
-                // if no user is found, return the message
-                if (!user)
-                    return done(null, false, req.flash('loginMessage', 'No user found.'));
-
-                if (!user.validPassword(password))
-                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
-
-                // all is well, return user
-                else
-                    return done(null, user);
-            });
-        });
-
-    }));
-
-    // LOCAL SIGNUP ============================================================
-    
-    passport.use('local-signup', new LocalStrategy({
-        // override local strategy defaults username and password with email
-        usernameField : 'email',
-        passwordField : 'password',
-        passReqToCallback : true // check if a user is logged in or not
-    },
-    function(req, email, password, done) {
-
-        // asynchronous
-        process.nextTick(function() {
-
-            //  check if the email address is in use.
-            User.findOne({'local.email': email}, function(err, existingUser) {
-
-                // if there are any errors, return the error
-                if (err)
-                    return done(err);
-
-                // check if there's already a user with that email
-                if (existingUser) 
-                    return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-
-                //  If we're logged in, we're connecting a new local account.
-                if(req.user) {
-                    var user            = req.user;
-                    user.local.email    = email;
-                    user.local.password = user.generateHash(password);
-                    user.save(function(err) {
-                        if (err)
-                            throw err;
-                        return done(null, user);
-                    });
-                } 
-                //  We're not logged in, so we're creating a brand new user.
-                else {
-                    // create the user
-                    var newUser            = new User();
-
-                    newUser.local.email    = email;
-                    newUser.local.password = newUser.generateHash(password);
-
-                    newUser.save(function(err) {
-                        if (err)
-                            throw err;
-
-                        return done(null, newUser);
-                    });
-                }
-
-            });
-        });
-
-    }));
-
-   
